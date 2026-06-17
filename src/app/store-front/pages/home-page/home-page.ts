6702d8b681269cde85e06044ae4cd3fb9ca12362
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2026-06-17 18:27:16 +0200

    Get products list

--- src/app/store-front/pages/home-page/home-page.ts
+++ src/app/store-front/pages/home-page/home-page.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
-import { ProductCard } from "../../components/product-card/product-card";
+import { Component, inject } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { ProductCard } from '../../components/product-card/product-card';
+import { ProductsService } from '../../../products/services/products.service';
 
 @Component({
   selector: 'app-home-page',
   imports: [ProductCard],
   templateUrl: './home-page.html',
 })
-export class HomePage {}
+export class HomePage {
+  productsService = inject(ProductsService);
+
+  productsResource = rxResource({
+    params: () => ({}),
+    stream: () => {
+      return this.productsService.getProducts();
+    },
+  });
+}
