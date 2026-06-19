a75aaf61f8c9f5fc2d15807ac1b1c5ba28cca691
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2026-06-19 11:37:36 +0200

    Create products page by gender

--- src/app/store-front/pages/gender-page/gender-page.ts
+++ src/app/store-front/pages/gender-page/gender-page.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs';
+import { ProductsService } from '../../../products/services/products.service';
+import { ProductCard } from "../../components/product-card/product-card";
 
 @Component({
   selector: 'app-gender-page',
-  imports: [],
+  imports: [ProductCard],
   templateUrl: './gender-page.html',
 })
-export class GenderPage {}
+export class GenderPage {
+  route = inject(ActivatedRoute);
+  productsService = inject(ProductsService);
+
+  gender = toSignal(this.route.params.pipe(map(({ gender }) => gender)));
+
+  productsResource = rxResource({
+    params: () => ({ gender: this.gender() }),
+    stream: ({ params }) => this.productsService.getProducts({ gender: params.gender }),
+  });
+}
