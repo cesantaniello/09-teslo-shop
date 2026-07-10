bce13a74f13a7c46dd47f9bafbdd1588e82f285e
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2026-07-10 14:02:46 +0200

    Create pagination with RxResource

--- src/app/store-front/pages/home-page/home-page.ts
+++ src/app/store-front/pages/home-page/home-page.ts
@@ -1,8 +1,10 @@
-import { Component, inject } from '@angular/core';
-import { rxResource } from '@angular/core/rxjs-interop';
+import { Component, inject, resource } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { ProductCard } from '../../components/product-card/product-card';
 import { ProductsService } from '../../../products/services/products.service';
-import { Pagination } from "../../../shared/components/pagination/pagination/pagination";
+import { Pagination } from '../../../shared/components/pagination/pagination/pagination';
+import { ActivatedRoute } from '@angular/router';
+import { firstValueFrom, map } from 'rxjs';
 
 @Component({
   selector: 'app-home-page',
@@ -12,7 +14,23 @@ import { Pagination } from "../../../shared/components/pagination/pagination/pag
 export class HomePage {
   productsService = inject(ProductsService);
 
-  productsResource = rxResource({
-    stream: () => this.productsService.getProducts({ limit: 20 }),
+  activatedRoute = inject(ActivatedRoute);
+
+  currentPage = toSignal(
+    this.activatedRoute.queryParamMap.pipe(
+      map((params) => Number(params.get('page')) || 1),
+      map((page) => (Number.isNaN(page) ? 1 : page)),
+    ),
+    { initialValue: 1 },
+  );
+
+  productsResource = resource({
+    params: () => ({ page: this.currentPage() }),
+    loader: ({ params }) =>
+      firstValueFrom(
+        this.productsService.getProducts({
+          offset: (params.page - 1) * 9,
+        }),
+      ),
   });
 }
