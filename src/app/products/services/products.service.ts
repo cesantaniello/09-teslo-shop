230e7b5e6b52ada8460f2df9482e68a8519e3082
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2026-07-13 13:56:01 +0200

    Create products cache

--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { Observable, of, tap } from 'rxjs';
 import { Product, ProductsResponse } from '../interfaces/product.interface';
 import { environment } from '../../../environments/environment';
 
@@ -16,14 +16,27 @@ interface Options {
 export class ProductsService {
   private http = inject(HttpClient);
 
+  private productsCache = new Map<string, ProductsResponse>();
+
   getProducts(options: Options): Observable<ProductsResponse>{
     const { limit = 9, offset = 0, gender = ''} = options;
+
+    const key = `limit=${limit}&offset=${offset}&gender=${gender}`;
 
     const params: Record<string, string | number> = { limit, offset };
     if (gender) params['gender'] = gender;
 
+    if (this.productsCache.has(key)) {
+      return of(this.productsCache.get(key)!);
+    }
+
     return this.http.get<ProductsResponse>(`${baseUrl}/products`,
-      { params: {limit, offset, gender,},}).pipe(tap((resp) => console.log(resp)));
+      { params: {limit, offset, gender,},}).pipe(
+        tap((resp) => {
+          this.productsCache.set(key, resp);
+          console.log(resp);
+        }),
+      );
   }
 
   getProductByIdSlug(idSlug: string): Observable<Product>{
