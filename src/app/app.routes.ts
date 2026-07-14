248481f50a1d61619537489777f12375da5f0020
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2026-07-14 15:38:05 +0200

    Create login, routes and authentication pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,10 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.route').then(m => m.authRoutes)
+  },
+  {
     path: '',
     loadChildren: () => import('./store-front/store-front.routes')
   }
