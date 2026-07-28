4532dcb3566b93d17253fc5792eaf6befa044f19
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2026-07-28 11:19:05 +0200

    Create Guard and not Authenticated Guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
+import { NotAuthenticatedGuard } from './auth/guards/not-authenticated.guard';
 
 export const routes: Routes = [
   {
     path: 'auth',
-    loadChildren: () => import('./auth/auth.route').then(m => m.authRoutes)
+    loadChildren: () => import('./auth/auth.route'),
+    canMatch: [NotAuthenticatedGuard,
+      //() => {console.log('NotAuthenticatedGuard'); return true;}
+    ]
   },
   {
     path: '',
