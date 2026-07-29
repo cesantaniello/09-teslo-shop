9163e44634f024fded218200fdc197a63b47b63c
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2026-07-29 14:00:11 +0200

    Create Authorization

--- src/app/admin-dashboard/admin-dashboard.routes.ts
+++ src/app/admin-dashboard/admin-dashboard.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from "@angular/router";
 import { AdminDashboardLayout } from "./layouts/admin-dashboard-layout/admin-dashboard-layout";
 import { ProductAdminPage } from "./pages/product-admin-page/product-admin-page";
 import { ProductsAdminPage } from "./pages/products-admin-page/products-admin-page";
+import { isAdminGuard } from "../auth/guards/is-admin.guard";
 
 export const adminDashboardRoutes: Routes = [{
   path: '',
   component: AdminDashboardLayout,
+  canMatch: [isAdminGuard],
   children: [
     {
       path: 'products',
