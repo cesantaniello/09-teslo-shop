a8b0a5d97cdbf136500579f8dd1d2a020665f49d
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2026-07-30 15:31:06 +0200

    Show Products list

--- src/app/shared/components/product-table/product-table.ts
+++ src/app/shared/components/product-table/product-table.ts
@@ -1,9 +1,12 @@
 import { Component, input } from '@angular/core';
 import { Product } from '../../../products/interfaces/product.interface';
+import { ProductImagePipe } from "../../../products/pipes/product-image-pipe";
+import { RouterLink } from '@angular/router';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'product-table',
-  imports: [],
+  imports: [ProductImagePipe, RouterLink, CurrencyPipe],
   templateUrl: './product-table.html',
 })
 export class ProductTable {
