bed20b762d1186f3119221085aa79f4d3e06125a
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2026-08-06 15:32:33 +0200

    Show form errors

--- src/app/admin-dashboard/pages/product-admin-page/product-details/product-details.ts
+++ src/app/admin-dashboard/pages/product-admin-page/product-details/product-details.ts
@@ -3,10 +3,11 @@ import { Product } from '../../../../products/interfaces/product.interface';
 import { ProductCarousel } from "../../../../store-front/components/product-carousel/product-carousel";
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FormUtils } from '../../../../utils/form-utils';
+import { FormErrorLabel } from "../../../../shared/components/form-error-label/form-error-label";
 
 @Component({
   selector: 'product-details',
-  imports: [ProductCarousel, ReactiveFormsModule],
+  imports: [ProductCarousel, ReactiveFormsModule, FormErrorLabel],
   templateUrl: './product-details.html',
 })
 export class ProductDetails implements OnInit {
@@ -50,6 +51,7 @@ export class ProductDetails implements OnInit {
   }
 
   onSubmit() {
-    console.log(this.productForm.value)
+    const isValid = this.productForm.valid;
+    console.log(this.productForm.value, { isValid });
   }
 }
