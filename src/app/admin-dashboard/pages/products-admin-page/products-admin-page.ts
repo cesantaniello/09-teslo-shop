8be4124bc7ad80c1d9288490a17ef7e1e5556107
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2026-08-06 09:16:17 +0200

    Show paginated products

--- src/app/admin-dashboard/pages/products-admin-page/products-admin-page.ts
+++ src/app/admin-dashboard/pages/products-admin-page/products-admin-page.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject, resource, signal } from '@angular/core';
 import { ProductTable } from "../../../shared/components/product-table/product-table";
+import { firstValueFrom } from 'rxjs';
+import { ProductsService } from '../../../products/services/products.service';
+import { PaginationService } from '../../../shared/components/pagination/pagination/pagination.service';
+import { Pagination } from "../../../shared/components/pagination/pagination/pagination";
 
 @Component({
   selector: 'app-products-admin-page',
-  imports: [ProductTable],
+  imports: [ProductTable, Pagination],
   templateUrl: './products-admin-page.html',
 })
-export class ProductsAdminPage {}
+export class ProductsAdminPage {
+  productsService = inject(ProductsService);
+  paginationService = inject(PaginationService);
+
+  productsPerPage = signal(10);
+
+  productsResource = resource({
+    params: () => ({ 
+      page: this.paginationService.currentPage(), 
+      limit: this.productsPerPage() }),
+    loader: ({ params }) =>
+      firstValueFrom(
+        this.productsService.getProducts({
+          offset: (params.page - 1) * 9, limit: params.limit
+        }),
+      ),
+  });
+}
