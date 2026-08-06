3db5e36efcf6298fb7f7ef0134be9e9f942b3bf5
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2026-08-06 10:00:11 +0200

    Create product page by Id

--- src/app/admin-dashboard/pages/product-admin-page/product-admin-page.ts
+++ src/app/admin-dashboard/pages/product-admin-page/product-admin-page.ts
@@ -1,8 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute, Router } from '@angular/router';
+import { map } from 'rxjs';
+import { ProductsService } from '../../../products/services/products.service';
 
 @Component({
   selector: 'app-product-admin-page',
   imports: [],
   templateUrl: './product-admin-page.html',
 })
-export class ProductAdminPage {}
+export class ProductAdminPage {
+  activatedRoute = inject(ActivatedRoute);
+  router = inject(Router);
+  productsService = inject(ProductsService);
+
+  productId = toSignal(
+    this.activatedRoute.params.pipe(map((params) => params['id']))
+  );
+
+  productResource = rxResource({
+    params: () => ({ id: this.productId() }),
+    stream: ({ params }) => 
+      this.productsService.getProductById(params.id),
+  });
+
+  redirectEffect = effect(() => {
+    const resource = this.productResource;
+    if (resource.error()) {
+      this.router.navigate(['/admin/products']);
+    }
+  });
+}
--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -51,4 +51,14 @@ export class ProductsService {
     );
   }
 
+    getProductById(id: string): Observable<Product>{
+    if (this.productCache.has(id)) {
+      return of(this.productCache.get(id)!);
+    }
+    return this.http.get<Product>(`${baseUrl}/products/${id}`).pipe(
+      tap((product) => {
+        this.productCache.set(id, product);
+      }),
+    );
+  }
 }
